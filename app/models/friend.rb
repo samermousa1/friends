7ac3b0f5c1386577a6f47181766714910349306c
Samer MOUSSA <samermousa1@hotmail.com>
2023-01-28 20:39:41 +0100

    Adding validation for Email and First_name, back/front end

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -1,3 +1,5 @@
 class Friend < ApplicationRecord
     belongs_to :user
+    validates_presence_of :email, :first_name , class: :friend
+    validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create
 end
