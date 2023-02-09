60fd01811c73dfb514635cb992b05f9efdd843f4
Samer MOUSSA <samermousa1@hotmail.com>
2023-02-09 21:14:46 +0100

    Add invite unieqness

--- app/models/invite.rb
+++ app/models/invite.rb
@@ -1,5 +1,6 @@
 class Invite < ApplicationRecord
     STATUS_TYPES = {"pending" => 1, "accepted" => 2, "rejected" => 3}.freeze
     enum status: STATUS_TYPES
+    validates :inviter_id, uniqueness: { scope: :invitee_id, message: "invite already sent" }
 end
   
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :friends       
+  has_many :friends   
+  has_many :invites, class_name: "Invite", foreign_key: "inviter_id"
 end
